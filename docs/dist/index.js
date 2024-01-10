0760e6b31e7d6b181124d60e88d43575c1870ff2
Harrison Hong <hhong19@asu.edu>
2024-01-10 16:40:10 -0700

    added drag and drop to build

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -1,10 +1,77 @@
 import {v4 as uuidV4} from "../_snowpack/pkg/uuid.js";
+import $ from "../_snowpack/pkg/jquery.js";
 const list = document.querySelector("#list");
 const form = document.getElementById("new-task-form");
 const input = document.querySelector("#new-task-title");
 const tasks = loadTasks();
-tasks.forEach(addListItem);
-tasks.forEach(checkCompleted);
+let dragStartIndex;
+let trailDetect = false;
+createList();
+function createList() {
+  tasks.forEach(addListItem);
+  tasks.forEach(checkCompleted);
+  addDragEventListeners();
+}
+function dragStart() {
+  dragStartIndex = $(this.closest("li")).index();
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragEnterTrailing() {
+  this.classList.add("over");
+  trailDetect = true;
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+function dragLeaveTrailing() {
+  this.classList.remove("over");
+  trailDetect = false;
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragDrop() {
+  let dragEndIndex;
+  if (trailDetect) {
+    dragEndIndex = tasks.length;
+    trailDetect = false;
+  } else {
+    dragEndIndex = $(this).index();
+  }
+  reorderItem(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
+function reorderItem(fromIndex, toIndex) {
+  const item = tasks[fromIndex];
+  tasks.splice(toIndex, 0, item);
+  if (fromIndex > toIndex)
+    fromIndex++;
+  tasks.splice(fromIndex, 1);
+  saveTasks();
+  location.reload();
+}
+function addDragEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const listItems = document.querySelectorAll("#list li");
+  const trailing = document.querySelectorAll("#trailing");
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+  listItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+  trailing.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnterTrailing);
+    item.addEventListener("dragleave", dragLeaveTrailing);
+  });
+}
 form?.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input?.value == "" || input?.value == null)
@@ -24,6 +91,8 @@ function addListItem(task) {
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
   const removeBtn = document.createElement("span");
+  item.setAttribute("draggable", "true");
+  item.classList.add("draggable");
   checkbox.addEventListener("change", () => {
     task.completed = checkbox.checked;
     checkCompleted(task);
@@ -37,8 +106,8 @@ function addListItem(task) {
     removeTask(task);
   });
   label.id = task.id;
-  label.append(checkbox, task.title, removeBtn);
-  item.append(label);
+  label.append(checkbox, task.title);
+  item.append(label, removeBtn);
   list?.append(item);
   saveTasks();
 }
@@ -65,7 +134,6 @@ function removeTask(task) {
   tasks.forEach((value, index) => {
     if (tasks[index].id == id) {
       tasks.splice(index, 1);
-      console.log("Item index at " + index + " was removed.");
     }
   });
   saveTasks();
